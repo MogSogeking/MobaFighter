03579c44f8e877751ec73049c29bcce3d06d9726
Vincent Poesmann <vincent.poesmann@gmail.com>
2018-10-15 14:02:12 +0200

    Add hit movement, light and medium hit

--- Entities/Character.gd
+++ Entities/Character.gd
@@ -9,6 +9,9 @@ enum State {
 
 export (int) var speed
 
+signal staminaChanged
+signal lifeChanged
+
 var direction = Vector2()
 var velocity = Vector2()
 var state = State.walk
@@ -17,6 +20,7 @@ var framerate = 1.0/60.0
 var startup = 0
 var active = 0
 var recover = 0
+var stamina = 100
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -35,37 +39,57 @@ func processControls():
 			velocity.x -= speed
 			
 	if Input.is_action_just_pressed("action1"):
-		if $actionTimer.is_stopped():
+		if state != State.startHit && stamina > 10:
+			stamina -= 10
 			state = State.startHit
-			startup = 8
+			startup = 6
 			active = 4
 			recover = 8
-			$actionTimer.wait_time = framerate * startup
-			$actionTimer.start()
-			$Sprite.modulate = Color(0, 1, 0)
-			var targetPos = global_position * direction
-			print(targetPos)
-			$HitTween.interpolate_property(self, "transform:pos", global_position, targetPos, (startup+active+recover)*framerate, Tween.TRANS_QUINT, Tween.EASE_OUT)
+			attack()
+	
+	if Input.is_action_just_pressed("action2"):
+		if state != State.startHit && stamina > 20:
+			stamina -= 20
+			state = State.startHit
+			startup = 9
+			active = 6
+			recover = 12
+			attack()
 
+func attack():
+	$HitTween.stop(self)
+	$ActionTimer.wait_time = framerate * startup
+	$ActionTimer.start()
+	$Sprite.modulate = Color(0, 1, 0)
+	var targetPos = global_position + (direction * 20)
+	var tweenDuration = framerate * (startup + active)
+	$HitTween.interpolate_property(self, 'position', global_position, targetPos, tweenDuration, Tween.TRANS_BACK, Tween.EASE_OUT)
+	$HitTween.start()
+	print(global_position)
 
 func _physics_process(delta):
-	direction = get_angle_to(get_global_mouse_position())
+	direction = (get_global_mouse_position() - global_position).normalized() 
 	processControls()
 	move_and_slide(velocity)
 	velocity = Vector2()
 
-func _on_actionTimer_timeout():
-	$actionTimer.stop()
+func _on_ActionTimer_timeout():
+	$ActionTimer.stop()
 	if state == State.startHit:
 		$Sprite.modulate = Color(0, 0, 1)
 		state = State.activeHit
-		$actionTimer.wait_time = framerate * active
-		$actionTimer.start()
+		$ActionTimer.wait_time = framerate * active
+		$ActionTimer.start()
 	elif state == State.activeHit:
 		$Sprite.modulate = Color(1, 0, 0)
 		state = State.recoverHit
-		$actionTimer.wait_time = framerate * recover
-		$actionTimer.start()
+		$ActionTimer.wait_time = framerate * recover
+		$ActionTimer.start()
 	elif state == State.recoverHit:
 		$Sprite.modulate = Color(1, 1, 1)
 		state = State.walk
+
+
+func _on_StaminaTimer_timeout():
+	if stamina < 100:
+		stamina += 1
